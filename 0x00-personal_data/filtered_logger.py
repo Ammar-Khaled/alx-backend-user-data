b3ddeaaa5ad2b023b6ed3992c24d95c96a24ed57
Ammar-Khaled <ammar.khaled.github@gmail.com>
2024-01-11 15:08:31 +0000

    Implement filter_datum function

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Filtered logger."""
+from typing import List
+from re import sub
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """Return the log message obfuscated."""
+    for field in fields:
+        message = sub(f'{field}=.*?{separator}', f'{field}={redaction}{separator}', message)
+    return message
