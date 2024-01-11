aa10521883c8e12796678ce8cbdfeaa56e14c68d
Ammar-Khaled <ammar.khaled.github@gmail.com>
2024-01-11 16:38:59 +0000

    Complete tasks#1, 2: implement RedactingFormatter class and get_logger method

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,9 @@
 """Filtered logger."""
 from typing import List
 from re import sub
+import logging
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -11,3 +14,37 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = sub(field + "=.*?" + separator,
                       field + "=" + redaction + separator, message)
     return message
+
+
+def get_logger() -> logging.Logger:
+    """Create a Logger."""
+    formatter = RedactingFormatter(PII_FIELDS)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+
+    logger = logging.getLogger("user_data")
+    logger.propagate = False
+    logger.setLevel(logging.INFO)
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Init method."""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filter values in incoming log records."""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
